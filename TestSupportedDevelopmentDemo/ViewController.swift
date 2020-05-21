ebb98ca600343fbbf74c2cf0665c4ae459a91aae
Martin Höller <martin@martinhoeller.net>
2020-05-21 17:40:54 +0200

    Initial Commit

---
+++ TestSupportedDevelopmentDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestSupportedDevelopmentDemo
+//
+//  Created by Martin Höller on 21.05.20.
+//  Copyright © 2020 Martin Hoeller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TestSupportedDevelopmentDemoTests/TestSupportedDevelopmentDemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  TestSupportedDevelopmentDemoTests.swift
+//  TestSupportedDevelopmentDemoTests
+//
+//  Created by Martin Höller on 21.05.20.
+//  Copyright © 2020 Martin Hoeller. All rights reserved.
+//
+
+import XCTest
+@testable import TestSupportedDevelopmentDemo
+
+class TestSupportedDevelopmentDemoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
